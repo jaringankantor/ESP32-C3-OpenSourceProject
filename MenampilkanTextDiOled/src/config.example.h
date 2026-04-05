679029ccf8859af2367fef1e8091febfc112fcac
nc.anwar@JaringanKantor.com <nc.anwar@jaringankantor.com>
2026-04-05 10:35:49 +0700

    Refactor config.example.h for WiFi credentials and add new config.example.h for text display

--- MenampilkanTextDiOled/src/config.example.h
+++ MenampilkanTextDiOled/src/config.example.h
@@ -4,7 +4,6 @@
 // Salin file ini menjadi config.h lalu isi dengan kredensial asli.
 // Jangan commit config.h ke Git.
 
-#define WIFI_SSID "your_wifi_ssid"
-#define WIFI_PASSWORD "your_wifi_password"
+#define TextTampil "HALLO"
 
 #endif
